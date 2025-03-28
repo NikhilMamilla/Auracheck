0e6b49973189e1fa1aa95f5f183f0eebd369e218
Mamilla Nikhil <168348085+NikhilMamilla@users.noreply.github.com>
2025-03-28 12:08:25 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,23 @@
+// firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAmhms0W9_ENu0qin00G7MzF8_joqLpqPk",
   authDomain: "auracheck-f408f.firebaseapp.com",
   projectId: "auracheck-f408f",
-  storageBucket: "auracheck-f408f.firebasestorage.app",
+  storageBucket: "auracheck-f408f.appspot.com", // Fixed storage bucket URL
   messagingSenderId: "670711392230",
   appId: "1:670711392230:web:7e4c26fdc8e3e58a351827",
   measurementId: "G-KGEJHHPRR5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
